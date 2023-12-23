e1e8721ef0fcf78c2b0093e96af61cda7d0075e7
Tohy Ny Aina <109283824+TohyNyAina@users.noreply.github.com>
2023-12-23 08:26:54 +0300

    Update TeamChannelPreview.jsx

--- src/components/TeamChannelPreview.jsx
+++ src/components/TeamChannelPreview.jsx
@@ -8,10 +8,31 @@ const TeamChannelPreview = ({ channel, type }) => {
         <p className='channel-preview__item'>
             # {channel?.data?.name || channel?.data?.id}
         </p>
-    )
+    );
+
+    const DirectPreview = () => {
+        const members = Object.values(channel.state.members).filter(({ user }) => user.id !== client.userId );
+    
+        return (
+            <div className='channel-preview__item single'>
+                <Avatar 
+                    image={members[0]?.user?.image}
+                    name={members[0]?.user?.fullName}
+                    size={24}
+                />
+                <p>{members[0]?.user?.fullName}</p>
+            </div>
+        )
+    }
     
     return (
-        <div>
+        <div className={
+            channel?.id === activeChannel?.id
+                ? 'channel-preview__wrapper__selected'
+                : 'channel-preview__wrapper'
+        }
+        onClick={}
+        >
             
         </div>
     );
