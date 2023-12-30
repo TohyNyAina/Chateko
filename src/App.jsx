289900ce2e05b0cc3c7beaa996341f9e90d5e839
Tohy Ny Aina <109283824+TohyNyAina@users.noreply.github.com>
2023-12-30 11:09:48 +0300

    update

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ import Cookies from 'universal-cookie';
 
 import { ChannelListContainer, ChannelContainer, Auth } from './components';
 
+import 'stream-chat-react/dist/css/index.css';
 import './App.css';
 
 const cookies = new Cookies();
--- src/components/ChannelContainer.jsx
+++ src/components/ChannelContainer.jsx
@@ -3,37 +3,42 @@ import { Channel, useChatContext } from 'stream-chat-react';
 
 import { ChannelInner, CreateChannel, EditChannel, TeamMessage } from './';
 
-const ChannelContainer = ({ isCreating, setIscreating, setCreateType, setIsEditing, createType}) => {
-    const { Channel } = useChatContext();
+const ChannelContainer = ({ isCreating, setIsCreating, isEditing, setIsEditing, createType }) => {
+    const { channel } = useChatContext();
 
-    if(isCreating) {
-        return(
+    if (isCreating) {
+        return (
             <div className='channel__container'>
-                <CreateChannel createType={createType} setIscreating={isCreating} />
+                <CreateChannel createType={createType} setIsCreating={setIsCreating} />
             </div>
-        )
+        );
     }
 
-    if(isEditing) {
-        return(
+    if (isEditing) {
+        return (
             <div className='channel__container'>
                 <EditChannel setIsEditing={setIsEditing} />
             </div>
-        )
+        );
     }
 
-    const EmptyState = () => {
+    const EmptyState = () => (
         <div className='channel-empty__container'>
             <p className='channel-empty__first'>Ity no fiandohan'ny tantaran'ny chat anao.</p>
             <p className='channel-empty__second'>Mandefasa hafatra, rohy, sary kely maneho fihetseham-po, ary maro hafa!</p>
         </div>
-    }
+    );
 
     return (
-        <div>
-            ChannelContainer
+        <div className='channel__container'>
+            <Channel
+                EmptyStateIndicator={EmptyState}
+                Message={(messageProps, i) => <TeamMessage key={i} {...messageProps} />}
+            >
+                <ChannelInner setIsEditing={setIsEditing} />
+            </Channel>
         </div>
     );
-}
+};
 
 export default ChannelContainer;
