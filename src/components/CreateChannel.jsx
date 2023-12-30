3f96c57b6a2c5bb3fc8407c1edecc435729998ce
Tohy Ny Aina <109283824+TohyNyAina@users.noreply.github.com>
2023-12-30 12:13:00 +0300

    Update CreateChannel.jsx

--- src/components/CreateChannel.jsx
+++ src/components/CreateChannel.jsx
@@ -1,9 +1,29 @@
 import React from 'react';
+import { useChatContext } from 'stream-chat-react';
+
+import { UserList } from './'
+import { CloseCreateChannel } from '../assets';
+
+const ChannelNameInput = ({ channelName = '', setChannelName }) => {
+    const handleChange = (event) => {
+        event.preventDefault();
+        setChannelName(event.target.value);
+    }
+    
+    return (
+        <div className='channel-name-input__wrapper'>
+            <p>Anarana</p>
+            <input value={channelName} onChange={handleChange} placeholder="anaran'ny fikambanana" />
+            <p>Hampiditra mpikambana</p>
+        </div>
+    );
+}
+
 
 const CreateChannel = () => {
     return (
         <div>
-            CreateChannel
+            <ChannelNameInput />
         </div>
     );
 }
