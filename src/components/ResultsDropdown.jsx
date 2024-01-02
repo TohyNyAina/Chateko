c06ddd897963fe932bc48bac43d374004c055aba
Tohy Ny Aina <109283824+TohyNyAina@users.noreply.github.com>
2024-01-02 17:00:46 +0300

    Update ResultsDropdown.jsx

--- src/components/ResultsDropdown.jsx
+++ src/components/ResultsDropdown.jsx
@@ -61,15 +61,15 @@ const ResultsDropdown = ({ teamChannels, directChannels, focusedId, loading, set
 
   return (
     <div className='channel-search__results'>
-      <p className='channel-search__results-header'>Channels</p>
+      <p className='channel-search__results-header'>Fantsona</p>
       {loading && !teamChannels.length && (
         <p className='channel-search__results-header'>
-          <i>Loading...</i>
+          <i>Miandry fa gasy...</i>
         </p>
       )}
       {!loading && !teamChannels.length ? (
         <p className='channel-search__results-header'>
-          <i>No channels found</i>
+          <i>Tsy misy fantsona hita</i>
         </p>
       ) : (
         teamChannels?.map((channel, i) => (
@@ -83,15 +83,15 @@ const ResultsDropdown = ({ teamChannels, directChannels, focusedId, loading, set
           />
         ))
       )}
-      <p className='channel-search__results-header'>Users</p>
+      <p className='channel-search__results-header'>Mpampiasa</p>
       {loading && !directChannels.length && (
         <p className='channel-search__results-header'>
-          <i>Loading...</i>
+          <i>Miandry fa gasy...</i>
         </p>
       )}
       {!loading && !directChannels.length ? (
         <p className='channel-search__res ults-header'>
-          <i>No direct messages found</i>
+          <i>Tsy misy mpampiasa hita</i>
         </p>
       ) : (
         directChannels?.map((channel, i) => (
