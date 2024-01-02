5d4a98df4a7f1d3ab125c8db886064e0a4d0d259
Tohy Ny Aina <109283824+TohyNyAina@users.noreply.github.com>
2024-01-02 15:18:35 +0300

    Update UserList.jsx

--- src/components/UserList.jsx
+++ src/components/UserList.jsx
@@ -16,14 +16,19 @@ const ListContainer = ({ children }) => {
 }
 
 const UserItem = ({ user }) => {
+    const [selected, setSelected] = useState(false)
+
+    const handleSelect = () => {
+        setSelected((prevSelected) => !prevSelected)
+    }
+
     return (
-        <div className='user-item__wrapper'>
+        <div className='user-item__wrapper' onClick={handleSelect}>
             <div className='user-item__name-wrapper'>
                 <Avatar image={user.image} name={user.fullName || user.id} size={32} />
                 <p className='user-item__name'>{user.fullName || user.id}</p>
             </div>
-            <InviteIcon />
-            <div/>
+            {selected ? <InviteIcon /> : <div className='user-item__invite-empty' />}
         </div>
     )
 }
