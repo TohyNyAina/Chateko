9e94dad80f364ba73c595fba26b495133c187e9f
Tohy Ny Aina <109283824+TohyNyAina@users.noreply.github.com>
2024-01-05 09:49:35 +0300

    up

--- src/components/ChannelContainer.jsx
+++ src/components/ChannelContainer.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { Channel, MessageSimple } from 'stream-chat-react';
 
 import { ChannelInner, CreateChannel, EditChannel } from './';
+import Spinner from '../Spinner';
 
 const ChannelContainer = ({ isCreating, setIsCreating, isEditing, setIsEditing, createType }) => {
     if(isCreating) {
